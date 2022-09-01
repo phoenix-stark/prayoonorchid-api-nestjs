f47dfd94e41a785b80f940b39213c6fe9797fa08
Thunyasit Pholprasit <thunyasitpholprasit@Thunyasit-MBP-2.local>
2022-09-01 21:14:23 +0700

    export excel receipt

--- src/receipt/receipt.module.ts
+++ src/receipt/receipt.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from 'src/customer/entity/customer-entity.model';
+import { PlantFamilyMain } from 'src/plant-family-main/entity/plant-family-main-entity.model';
 import { Receipt } from './entity/receipt-entity.model';
 import { ReceiptController } from './receipt.controller';
 import { ReceiptService } from './receipt.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Receipt])],
+  imports: [TypeOrmModule.forFeature([Receipt, Customer, PlantFamilyMain])],
   controllers: [ReceiptController],
   providers: [ReceiptService],
   exports: [ReceiptService],
