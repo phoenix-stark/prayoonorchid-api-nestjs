7162de502313049e860bd23fd59697c29b0c06fa
Thunyasit Pholprasit <thunyasitpholprasit@Thunyasit-MBP.local>
2025-07-26 20:52:00 +0700

    fix: where employee

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -3008,24 +3008,27 @@ export class ReportService {
         });
       }
     } else {
-      query.andWhere('receipt_tb.code  LIKE :code ', {
-        code: `%%`,
-      });
-    }
+      // query.andWhere('receipt_tb.code  LIKE :code ', {
+      //   code: `%%`,
+      // });
 
-    // Receipt Name
-    if (filterReceiptName && filterReceiptName != '') {
-      if (filterReceiptNameIsMatchAll + '' == 'true') {
-        query.andWhere('receipt_tb.name  LIKE :receipt_name ', {
-          receipt_name: `${filterReceiptName}`,
-        });
+      // Receipt Name
+      if (filterReceiptName && filterReceiptName != '') {
+        if (filterReceiptNameIsMatchAll + '' == 'true') {
+          query.andWhere('receipt_tb.name  LIKE :receipt_name ', {
+            receipt_name: `${filterReceiptName}`,
+          });
+        } else {
+          query.andWhere('receipt_tb.name  LIKE :receipt_name ', {
+            receipt_name: `%${filterReceiptName}%`,
+          });
+        }
       } else {
-        query.andWhere('receipt_tb.name  LIKE :receipt_name ', {
-          receipt_name: `%${filterReceiptName}%`,
+        query.andWhere('receipt_tb.code  LIKE :code ', {
+          code: `%%`,
         });
       }
     }
-
     // FOOD PLANT
     if (filterFoodPlantDesc && filterFoodPlantDesc != '') {
       if (filterFoodPlantDescIsMatchAll + '' == 'true') {
